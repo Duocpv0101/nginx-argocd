fa5633a46eb780a423ceab568ccfb49d7a29b90d
DuocPVTanaakk <pham.van.duoc@tanaakk.com>
2024-06-19 09:48:33 +0700

    add name

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Engineer DuocPV!"
+    return "Hi, Engineer DuocPV!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
